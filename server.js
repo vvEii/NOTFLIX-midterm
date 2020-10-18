30148fae99cb9fe76c782222006ed44da5e5edb3
Ife Olaifa <ifeolaifa5@gmail.com>
2020-10-18 21:04:35 +0000

    removed old  routes and added seperated routes

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require('cookie-session');
 const sass = require("node-sass-middleware");
-const bcrypt = require('bcrypt');
 const app = express();
 const morgan = require("morgan");
 
@@ -18,7 +17,7 @@ const dbParams = require("./lib/db.js");
 const db = new Pool(dbParams);
 db.connect();
 //test the database connection
-db.query("SELECT * FROM test;").then((res) => console.log(res.rows));
+//db.query("SELECT * FROM test;").then((res) => console.log(res.rows));
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -43,11 +42,18 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const loginRoutes = require("./routes/login");
+const registrationRoutes = require("./routes/registration");
+// const logoutRoutes =  require("./routes/logout");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use('/login', loginRoutes(db));
+app.use('/register', registrationRoutes(db));
+// app.use('/logout', logoutRoutes(db));
+
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -56,75 +62,6 @@ app.use("/api/widgets", widgetsRoutes(db));
 app.get("/", (req, res) => {
   res.render("index");
 });
-
-//get register
-app.get('/register', (req, res) => {
-  res.render('register_url');
-});
-
-app.get('/login', (req, res) => {
-  res.render('login_url');
-});
-
-// register post 
-app.post('/register', (req, res) => {
-  const { username, password, email, phone } = req.body;
-  const hashedPassword = bcrypt.hashSync(password, 10);
-  const userValues = [username, hashedPassword, email, phone];
-
-  //  req.session.user_id = email;
-
-  //checks if info is present already 
-  const queryCheck = `
-  SELECT username, password, email, phone_number 
-  FROM users 
-  WHERE username = $1 AND password =$2 AND email =$3 AND phone_number =$4;
-  `;
-
-  db.query(queryCheck, userValues)
-    .then(result => {
-      if (result.rows.length > 0) {
-        res.status(400).send("Email is already in the system!");
-      } else {
-        //if info is not present then insert information
-        // no administrator present cuz set to default in database
-        const queryString = `INSERT INTO users (username, password, email, phone_number) VALUES ($1, $2, $3, $4) RETURNING *;`;
-        db.query(queryString, userValues)
-          .then(result => {
-            req.session.user_id = result.rows[0].email; //-> not working
-            // redirect to home listing --> change
-            res.redirect('/');
-          });
-      }
-    });
-});
-
-// login post 
-app.post('/login', (req, res) => {
-  const {email, password} = req.body;
-  const values = [email, password];
-  
-  const queryString = `
-  SELECT email, password
-  FROM users 
-  WHERE email = $1 AND password = $2;
-  `; 
-
-  db.query(queryString, values)
-  .then(result => {
-    const passwordCompare = bcrypt.compareSync(password, result.rows[0].password);
-    const emailCompare = (email!== result.rows[0].email); 
-
-    if(emailCompare && !passwordCompare) {
-      res.status(403).send("Email or Password don't match");
-    } else {
-      req.session.user_id = result.rows[0].email;
-      // redirect to home listing --> change
-      res.redirect('/');
-    }
-  });
-});
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
