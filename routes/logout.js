de6a9b492fc13fce252543aa2b4d063d10d976cf
Ife Olaifa <ifeolaifa5@gmail.com>
2020-10-18 21:05:51 +0000

    seperate route file for logout

---
+++ routes/logout.js
@@ -0,0 +1,11 @@
+
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.post('/', (req, res) => {
+    req.session.userId = null;
+    res.send({});
+  });
+}
