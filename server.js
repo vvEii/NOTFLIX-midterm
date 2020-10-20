592d94a3da72a01fbbd13499a09059ef2cc9044f
mohamedsa1990 <mohamedsa1990@gmail.com>
2020-10-20 02:54:24 +0000

    added add item page

--- server.js
+++ server.js
@@ -35,11 +35,16 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const addItemRoutes = require("./routes/addItem");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+
+app.use("/add", addItemRoutes(db));
+
+
 // Note: mount other resources here, using the same pattern above
 
 
